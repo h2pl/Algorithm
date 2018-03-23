15300c95f6cbdc4aabe93b38e11b1a7988fb8c3f
724888 <362294931@qq.com>
2018-03-23 13:30:46 +0800

    modify format

--- src/Basic/binarytree/levelprint.java
+++ src/Basic/binarytree/levelprint.java
@@ -3,21 +3,6 @@ package Basic.binarytree;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * Created by 周杰伦 on 2017/3/10.
- */
-class TreeNode{
-    TreeNode left=null;
-            TreeNode right=null;
-    int val;
-    TreeNode(int val){
-        this.val=val;
-
-    }
-    TreeNode(){
-        val=0;
-    }
-        }
 public class levelprint {
     public static void initTree(TreeNode root){
         root.val=0;
--- src/Format/input.java
+++ src/Format/input.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Created by 周杰伦 on 2017/3/29.
  */
 public class input {
-    public static void main(String[] args) {
+    public static void 输入两个数(String[] args) {
         Scanner input = new Scanner(System.in);
         String temp = input.nextLine();
         String[] ss = temp.trim().split(" ");
@@ -19,10 +19,23 @@ public class input {
         input.close();
 
 
-
     }
 
-    void SomeNumOneLine(){
+    public static void 输入一个数n和一个大小为n的数组(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String temp = input.nextLine();
+        String[] ss = temp.trim().split(" ");
+        int n = Integer.parseInt(ss[0]);
+        String temp2 = input.nextLine();
+        String[] ss2 = temp2.trim().split(" ");
+
+        int []num=new int[n];
+        for(int i=0;i<ss2.length;i++){
+            num[i]=Integer.parseInt(ss2[i]);
+        }
+    }
+
+    public static void 输入一个数组(){
 
         Scanner input = new Scanner(System.in);
         String temp = input.nextLine();
