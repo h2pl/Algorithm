fa8d80a73537dab274e33dc4c7c611c7afc4f462
724888 <362294931@qq.com>
2018-04-05 20:02:30 +0800

    腾讯笔试题

---
+++ src/In2018/Written/腾讯/input.java
@@ -0,0 +1,7 @@
+package In2018.Written.腾讯;
+
+/**
+ * Created by 周杰伦 on 2018/4/5.
+ */
+public class input {
+}
--- src/In2018/Written/腾讯/前端/Tencent6.java
+++ src/In2018/Written/腾讯/前端/Tencent6.java
@@ -25,6 +25,7 @@ public class Tencent6 {
 				break;
 			}
 		}
+
 		System.out.println(res);
 	}
 }
